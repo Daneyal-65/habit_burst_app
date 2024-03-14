77cc81fa3b4f10e1f91748c45784badb178daa01
daneyal <danixchacha@gmail.com>
2024-03-14 22:15:55 +0530

    url2

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // export const baseUrl = 'http://localhost:3002';
-export const baseUrl = "not-server-ssr.onrender.com";
+export const baseUrl = "https://not-server-ssr.onrender.com";
 
 // 'not-server-ssr.onrender.com'
