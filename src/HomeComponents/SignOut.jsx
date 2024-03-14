6a9de100a0d6449f22c2adf7d2b72b459b1509b8
daneyal <danixchacha@gmail.com>
2024-03-15 04:54:27 +0530

    bugfix

--- src/HomeComponents/SignOut.jsx
+++ src/HomeComponents/SignOut.jsx
@@ -2,17 +2,15 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 const SignOut = ({ onClick }) => {
-  const navigator = useNavigate();
   const handleClick = () => {
     localStorage.removeItem("userInfo");
     onClick(null);
-    navigator("/");
   };
 
   return (
     <>
       <button
-        onClick={() => handleClick}
+        onClick={handleClick}
         className="border-2 border-current w-max px-4 h-8 hover:bg-slate-500 rounded-2xl"
       >
         SignOut
